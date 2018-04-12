82168cc77933e6486c84a2afecda1d99e7468e9c
abc02 <abcgg02@gmail.com>
2018-04-12 14:23:42 +0800

    color #f6655e -> #399af7 & update siwper.vue

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -12,19 +12,3 @@ export default {
   props: ['slotNames', 'swiperHeight']
 }
 </script>
-<style>
-.el-carousel__button {
-  width: 18px;
-  height: 6px;
-  background-color: rgba(0, 0, 0, .6);
-}
-.el-carousel__indicator.is-active .el-carousel__button{
-  background-color: #f6655e !important;
-}
-.el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-}
-.el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-}
-</style>
